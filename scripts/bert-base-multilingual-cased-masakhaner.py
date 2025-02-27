e1781528cab4c920cf3a276a770157e26eea496b
Prosper Arineitwe Asiimwe <prox0591@gmail.com>
2025-02-27 21:57:20 +0200

    bert-base-multilingual-cased-masakhaner

---
+++ scripts/bert-base-multilingual-cased-masakhaner.py
@@ -0,0 +1,8 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+tokenizer = AutoTokenizer.from_pretrained("Davlan/bert-base-multilingual-cased-masakhaner")
+model = AutoModelForTokenClassification.from_pretrained("Davlan/bert-base-multilingual-cased-masakhaner")
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+example = "Emir of Kano turban Zhang wey don spend 18 years for Nigeria"
+ner_results = nlp(example)
+print(ner_results)
