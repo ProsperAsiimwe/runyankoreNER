fda861af9f2a185d93dbda62049765d57673dd1e
Prosper Arineitwe Asiimwe <prox0591@gmail.com>
2025-07-29 11:43:45 +0200

    generate latex tables script part 3

--- rq2_train_scripts/Embeddings/generate_similarity_latex_table_entity_only_embeddings.py
+++ rq2_train_scripts/Embeddings/generate_similarity_latex_table_entity_only_embeddings.py
@@ -21,6 +21,8 @@ def generate_latex_table(embedding_name, scores, languages, output_filename):
                 row.append(str(score))
         return " & ".join(row) + " \\\\"
 
+    total_cols = len(sample_sizes) * len(datasets)
+
     table_lines = []
     table_lines.append("\\begin{landscape}")
     table_lines.append("\\begin{table}[h!]")
@@ -29,14 +31,22 @@ def generate_latex_table(embedding_name, scores, languages, output_filename):
     table_lines.append(f"\\caption{{Mean Cosine Similarity Scores Between Runyankore and Other Languages Across Datasets and Sentence Sample Sizes ({embedding_name}, Full-Text Embeddings)}}")
     table_lines.append(f"\\label{{tab:cosine_similarity_comparison_{embedding_name.lower().replace('-', '')}}}")
     table_lines.append("\\begin{adjustbox}{max width=\\linewidth}")
-    table_lines.append("\\begin{tabular}{l*" + str(len(sample_sizes) * len(datasets)) + "{c}}")
+    table_lines.append("\\begin{tabular}{" + "l" + "c" * total_cols + "}")
     table_lines.append("\\toprule")
+
+    # First row: Multicolumn for each sample size
     table_lines.append("\\textbf{Lang} & " + " & ".join(
         [f"\\multicolumn{{3}}{{c}}{{\\textbf{{{size} Sent.}}}}" for size in sample_sizes]) + " \\\\")
-    table_lines.append("".join(["\\cmidrule(lr){" + f"{i*3+2}-{i*3+4}" + "}" for i in range(len(sample_sizes))]))
+
+    # Second row: cmidrules
+    cmidrule_parts = [f"\\cmidrule(lr){{{i*3+2}-{i*3+4}}}" for i in range(len(sample_sizes))]
+    table_lines.append("".join(cmidrule_parts))
+
+    # Third row: Datasets
     table_lines.append(" & " + " & ".join(datasets * len(sample_sizes)) + " \\\\")
     table_lines.append("\\midrule")
 
+    # Data rows
     for lang in sorted(languages):
         table_lines.append(format_row(lang))
 
@@ -71,5 +81,5 @@ for emb_label, emb_filename in embedding_variants.items():
                 scores[lang][sample][dataset] = sim
                 languages.add(lang)
 
-    output_tex_file = f"embeddings_only_cosine_similarity_table_{emb_label.lower().replace('-', '')}.tex"
+    output_tex_file = f"entity_only_cosine_similarity_table_{emb_label.lower().replace('-', '')}.tex"
     generate_latex_table(emb_label, scores, languages, output_tex_file)
